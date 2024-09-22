85cb875eddc8b1cc193b953c626e337de655fdcc
fxckcode <diegoale8582@hotmail.com>
2024-09-22 18:55:23 -0500

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,151 @@
+import indexTexts from '@/content/index'
+
+export default function Home() {
+  return (
+    <>
+       <nav
+            className="lg:w-[80%] sm:w-[85%] w-full h-14 flex justify-between items-center fixed top-0 backdrop-blur-sm px-5"
+        >
+            <ul className="flex flex-row gap-5">
+                <li
+                    className="hover:translate-y-1 hover:font-semibold transition-all text-sm md:text-base"
+                >
+                    <a href="#politicas">Política de privacidad</a>
+                </li>
+                <li
+                    className="hover:translate-y-1 hover:font-semibold transition-all text-sm md:text-base"
+                >
+                    <a href="#terminos">Términos y condiciones</a>
+                </li>
+            </ul>
+            <a
+                href="/auth/login"
+                className="p-2 rounded shadow bg-[#FF7A00] text-white font-semibold transition-all hover:-translate-y-1"
+                >Iniciar Sesión</a
+            >
+        </nav>
+        <div className="w-full flex justify-center h-full min-h-[100vh]">
+            <div
+                className="lg:w-[80%] sm:w-[85%] w-full h-full min-h-[100vh] flex justify-center items-center my-20 md:my-15"
+            >
+                <div className="w-full h-auto">
+                    <div className="w-full flex flex-row gap-2 lg:px-10 px-2">
+                        <div className="flex flex-col gap-2 w-full">
+                            <h1 className="font-bold text-2xl text-[#FF7A00]">
+                                {indexTexts.TITLE_HOME}<span
+                                    className="hover:animate-spin">👋</span
+                                >
+                            </h1>
+                            {
+                                indexTexts.CONTENT_HOME.map((t, index) => (
+                                    <p key={index}>{t.text}</p>
+                                ))
+                            }
+                            <strong
+                                >{indexTexts.STRONG_TEXT_CONTENT_HOME}</strong
+                            >
+                            <div
+                                className="w-full flex flex-row flex-wrap justify-center items-center gap-5"
+                            >
+                                {
+                                    indexTexts.TEXT_CONTENT_CARDS.map((t, index) => (
+                                        <div className="w-full max-w-md p-6 grid gap-6 border-[0.2px] border-gray-300 shadow-md rounded-lg transition-all hover:-translate-y-1 hover:border-[#ff7a00]" key={index}>
+                                            <div className="flex items-center gap-4">
+                                                <div className="bg-[#FF7A00] rounded-md p-3 flex items-center justify-center">
+                                                    <i
+                                                        className={`${t.icon} text-white`}
+                                                    />
+                                                </div>
+                                                <h4 className="text-2xl font-bold">
+                                                    {t.title}
+                                                </h4>
+                                            </div>
+                                            <p className="text-muted-foreground">
+                                                {t.description}
+                                            </p>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div
+            className="w-full flex justify-center h-full bg-[#FF7A00]"
+            id="politicas"
+        >
+            <div
+                className="lg:w-[65%] md:w-[75%] sm:w-[85%] w-full h-full flex justify-center items-center my-20"
+            >
+                <div className="w-full h-auto">
+                    <div
+                        className="flex flex-col gap-4 w-full text-white lg:px-10 px-2"
+                    >
+                        <h2 className="text-2xl font-bold text-center">
+                            Política de privacidad
+                        </h2>
+                        <div className="flex flex-col gap-3">
+                            {
+                                indexTexts.PRIVACY_POLICY_CONTENT.map((p, index) => (
+                                    <div className="flex flex-col gap-1" key={index}>
+                                        <strong>{p.title}</strong>
+                                        <p>{p.content}</p>
+                                        {p.subtext &&
+                                            p.subtext.map((t, keyIndex) => <p  key={keyIndex}>{t}</p>)}
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div
+            className="w-full flex flex-col items-center justify-center h-full"
+            id="terminos"
+        >
+            <div
+                className="lg:w-[65%] md:w-[75%] sm:w-[85%] w-full h-full flex justify-center items-center my-20"
+            >
+                <div className="w-full h-auto">
+                    <div className="flex flex-col gap-4 w-full lg:px-10 px-2">
+                        <h2 className="text-2xl font-bold text-center">
+                            {indexTexts.TERMS_AND_CONDITIONS_TITLE}
+                        </h2>
+                        <div className="flex flex-col gap-3">
+                            {
+                                indexTexts.TERMS_AND_CONDITIONS_CONTENT &&
+                                    indexTexts.TERMS_AND_CONDITIONS_CONTENT.map(
+                                        (t, index) => (
+                                            <div className="flex flex-col gap-1" key={index}>
+                                                <strong>{t.title}</strong>
+                                                {t.subtext.map((sub_t, keyIndex) => (
+                                                    <p key={keyIndex}>{sub_t}</p>
+                                                ))}
+                                            </div>
+                                        ),
+                                    )
+                            }
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <footer
+                className="w-full flex justify-center items-center shadow-lg h-10"
+            >
+                <p className="text-center font-bold text-[10px] md:text-base">
+                    Copyright © 2024 Naranja Erp. Todos los derechos
+                    reservados. <span
+                        className="hover:-translate-y-1 hover:scale-105 transition-all"
+                        ><a href="#politicas" className="text-[#FF7A00]"
+                            >Política de privacidad</a
+                        ></span
+                    >
+                </p>
+            </footer>
+        </div>
+    </>
+  );
+}
---
+++ src/content/index.ts
@@ -0,0 +1,175 @@
+const TITLE = 'NIARA | Asistente Virtual Financiero 🤖'
+const TITLE_HOME = "Bienvenida a NIARA! "
+const CONTENT_HOME = [
+    {
+        text: "¡Hola y bienvenida a NIARA! Un asistente virtual financiero con enfoque de género , desarrollado por NARANJA ERP AND ACADEMY SAS."
+    },
+    {
+        text: "Estamos encantadas de que te unas a nuestra comunidad de mujeres emprendedoras y freelancers que, como tú, desean llevar el control de sus finanzas personales y empresariales de manera organizada y eficiente."
+    },
+    {
+        text: "NIARA es un asistente virtual que funciona a través de WhatsApp. Para poder interactuar con NIARA, es necesario que el usuario tenga una cuenta de WhatsApp activa y que se encuentre en la lista de contactos de NIARA. Para poder ser agregado a la lista de contactos de NIARA, es necesario que el usuario se registre en la plataforma confirmando su número de teléfono y aceptando los términos y condiciones de uso de NIARA y debe cumplir con las políticas de uso de WhatsApp."
+    },
+    {
+        text: "Actualmente, NIARA se encuentra en versión alpha y estamos trabajando para mejorar su experiencia de uso. Próximamente estaremos lanzando la versión beta."
+    },
+    {
+        text: "NIARA es una plataforma diseñada especialmente para ti. Nuestro enfoque de género nos permite ofrecerte herramientas intuitivas y personalizadas que facilitarán tu planificación financiera, ayudándote a tomar decisiones informadas y alcanzar tus objetivos económicos con confianza y seguridad."
+    },
+    {
+        text: "En NIARA, cada emprendedora tiene a su alcance un instrumento poderoso que le ayudará a proyectar y controlar su futuro financiero, y el de su emprendimiento. Nuestro objetivo es apoyarte en cada paso del camino hacia el éxito financiero."
+    },
+    {
+        text: "Para comenzar, solo necesitas registrar tu nombre, apellido y correo electrónico. Te ofrecemos tanto una versión gratuita como una versión paga, para que elijas la que mejor se adapte a tus necesidades."
+    },
+    {
+        text: "Gracias por elegir NIARA. Estamos aquí para acompañarte y brindarte las herramientas que necesitas para triunfar."
+    }
+];
+
+const STRONG_TEXT_CONTENT_HOME = '¡Bienvenida a bordo!'
+
+const TEXT_CONTENT_CARDS = [
+    {
+        icon: "fa-solid fa-fire",
+        title: "Planificación Financiera Intuitiva",
+        description: "NIARA ofrece herramientas fáciles de usar que permiten a las usuarias planificar sus finanzas personales y empresariales de manera efectiva."
+    },
+    {
+        icon: "fa-regular fa-paper-plane",
+        title: "Decisiones Informadas",
+        description: "Las herramientas de NIARA proporcionan datos y análisis que ayudan a las emprendedoras a tomar decisiones financieras fundamentadas y seguras."
+    },
+    {
+        icon: "fa-solid fa-chart-simple",
+        title: "Control y Proyección Financiera",
+        description: "NIARA facilita el control de ingresos, gastos y deudas, permitiendo a las usuarias proyectar su futuro financiero y el de sus negocios con mayor precisión."
+    },
+    {
+        icon: "fa-solid fa-person-dress",
+        title: "Enfoque de Género",
+        description: "Diseñada específicamente para mujeres, NIARA entiende y responde a las necesidades particulares de las mujeres emprendedoras y freelancers."
+    },
+    {
+        icon: "fa-solid fa-check",
+        title: "Accesibilidad",
+        description: "Disponible en cualquier momento y desde cualquier lugar, permitiendo a las usuarias gestionar sus finanzas de forma conveniente y flexible."
+    }
+];
+
+const POLITICTS_TITLE = 'Política de privacidad'
+
+const PRIVACY_POLICY_CONTENT = [
+    {
+        title: "1. Introducción",
+        content: "En NARANJA ERP AND ACADEMY S.A.S., valoramos su privacidad y estamos comprometidos con la protección de sus datos personales. Esta política explica cómo recopilamos, utilizamos y protegemos su información personal cuando utiliza nuestro asistente financiero virtual NIARA a través de WhatsApp."
+    },
+    {
+        title: "2. ¿Qué información recopilamos?",
+        content: "Recopilamos información personal que usted nos proporciona directamente a través de WhatsApp, como su nombre, número de teléfono, dirección de correo electrónico, y otra información que usted elija proporcionar. También recopilamos información sobre cómo utiliza nuestro asistente financiero virtual NIARA, como las consultas que realiza y las respuestas que recibe."
+    },
+    {
+        title: "3. ¿Cómo utilizamos su información?",
+        content: "Utilizamos su información personal para proporcionarle mejores respuestas a sus consultas, mejorar nuestro asistente financiero virtual NIARA, y comunicarnos con usted sobre nuestros productos y servicios."
+    },
+    {
+        title: "4. ¿Compartimos su información?",
+        content: "NARANJA ERP AND ACADEMY S.A.S. no venderá, alquilará ni compartirá su información personal con terceros sin su consentimiento, excepto en los siguientes casos:\n- Para cumplir con leyes, regulaciones y procesos legales aplicables.\n- Para proteger los derechos y la seguridad de NARANJA ERP AND ACADEMY S.A.S., nuestros usuarios y el público."
+    },
+    {
+        title: "5. Seguridad de la Información",
+        content: "Implementamos medidas de seguridad adecuadas para proteger su información personal contra acceso no autorizado, alteración, divulgación o destrucción. Sin embargo, no podemos garantizar la seguridad absoluta de su información transmitida a través de Internet."
+    },
+    {
+        title: "6. Retención de Datos",
+        content: "Conservamos su información personal durante el tiempo necesario para cumplir con los fines para los cuales fue recopilada, incluyendo cualquier requerimiento legal, contable o de informes."
+    },
+    {
+        title: "7. Derechos del Usuario",
+        content: "De acuerdo con la Ley 1581 de 2012 y el Decreto 1377 de 2013, usted tiene derecho a: Conocer, actualizar y rectificar sus datos personales.",
+        subtext: [
+            '- Solicitar prueba de la autorización otorgada.',
+            '- Ser informado sobre el uso de sus datos personales.',
+            '- Presentar quejas ante la Superintendencia de Industria y Comercio.',
+            '- Revocar la autorización y/o solicitar la supresión de sus datos personales.'
+        ]
+    },
+    {
+        title: "8. Contacto",
+        content: "Si tiene alguna pregunta o inquietud sobre esta Política de Privacidad, puede contactarnos a través del correo electrónico oficial de NARANJA ERP AND ACADEMY S.A.S. para NIARA: correo de contacto INFO@NIARA.COM.CO\n\nEstas políticas están diseñadas para cumplir con las leyes y normativas colombianas, incluyendo la Ley 1581 de 2012 sobre Protección de Datos Personales y el Decreto 1377 de 2013."
+    },
+    {
+        title: "9. Cambios en la Política de Privacidad",
+        content: "Nos reservamos el derecho de modificar esta Política de Privacidad en cualquier momento. Le notificaremos cualquier cambio significativo a través de WhatsApp o por correo electrónico."
+    }
+];
+
+
+const TERMS_AND_CONDITIONS_TITLE = "Términos y Condiciones"
+
+const TERMS_AND_CONDITIONS_CONTENT = [
+    {
+        title: '1. Introducción',
+        subtext: ['Bienvenido a NIARA, un asistente financiero virtual desarrollado por NARANJA ERP AND ACADEMY S.AS. Al acceder y utilizar nuestros servicios a través de WhatsApp, usted acepta y está de acuerdo con estos Términos y Condiciones. Por favor, lea detenidamente estos términos antes de usar NIARA.']
+    },
+    {
+        title: '2. Uso de nuestros servicios',
+        subtext: [
+            'NIARA es una plataforma innovadora diseñada especialmente para mujeres emprendedoras y freelancers, que desean gestionar sus finanzas personales y empresariales, de manera organizada y eficiente.Este software con enfoque de género, ofrece herramientas intuitivas y personalizadas para facilitar la planificación financiera, lo que permite a sus usuarias tomar decisiones informadas para alcanzar sus objetivos económicos con confianza y seguridad.Con NIARA, cada emprendedora tiene a su alcance un instrumento que le ayudará a proyectar y controlar su futuro financiero, y el de su emprendimiento.',
+            'Las usuarias pueden acceder a NIARA a través de un registro que incluye su nombre, apellido y correo electrónico. Ofrecemos tanto una versión gratuita como una versión paga del servicio.',
+            'El servicio con versión gratuita ofrece funcionalidades básicas y una versión paga que incluye características avanzadas. Los detalles sobre los precios y las características de cada versión están disponibles en nuestra plataforma.'
+        ]
+    },
+    {
+        title: '3. Registro y Cuentas',
+        subtext: [
+            'Para utilizar NIARA es necesario registrarse proporcionando información personal precisa y completa, incluyendo nombre, apellido y correo electrónico. Estos datos serán tratados de acuerdo a nuestra a nuestra política de privacidad. Usted es responsable de mantener la confidencialidad de su cuenta y de todas las actividades que ocurran bajo su cuenta.'
+        ]
+    },
+    {
+        title: '4. Uso del servicio',
+        subtext: [
+            'El uso de NIARA está destinado únicamente a individuos mayores de edad con capacidad legal para celebrar contratos vinculantes. Usted se compromete a utilizar NIARA de manera legal y apropiada, respetando todas las leyes y regulaciones aplicables.',
+            'La usuaria asumirá la responsabilidad de todo lo que suceda con el celular que se haya utilizado para el registro en NIARA, por lo tanto, le corresponde realizar todas las acciones o denuncias pertinentes en caso de fraudes o plagios.',
+            'NIARA se reserva el derecho de admisión o no de la usuaria, en especial cuando se presenten comportamientos u omisiones que afecten el buen nombre de la plataforma. Por tanto, NIARA se reserva el derecho a limitar, suspender, bloquear o terminar su acceso a la plataforma, empleando cualquier recurso técnico necesario para ello.'
+        ]
+    },
+    {
+        title: '5. Modificaciones del Servicio',
+        subtext: [
+            'NARANJA ERP AND ACADEMY S.A.S. se reserva el derecho de modificar o descontinuar, temporal o permanentemente, el servicio (o cualquier parte del mismo) con o sin previo aviso. Usted acepta que NARANJA ERP AND ACADEMY S.A.S. no será responsable ante usted ni ante ningún tercero por cualquier modificación, suspensión o interrupción del servicio.'
+        ]
+    },
+    {
+        title: '6. Propiedad Intelectual',
+        subtext: [
+            'Todos los derechos de propiedad intelectual relacionados con NIARA, incluyendo pero no limitado a software, textos, gráficos, logotipos, y cualquier otro material, son propiedad de NARANJA ERP AND ACADEMY S.A.S. o sus licenciantes y están protegidos por las leyes de propiedad intelectual aplicables.',
+            'Usted no puede copiar, modificar, distribuir, vender o alquilar ninguna parte de NIARA, ni puede crear trabajos derivados basados en NIARA, sin el consentimiento previo por escrito de NARANJA ERP AND ACADEMY S.A.S.'
+        ]
+    },
+    {
+        title: '7. Limitación de Responsabilidad',
+        subtext: [
+            'En la máxima medida permitida por la ley, NARANJA ERP AND ACADEMY S.A.S. no será responsable por daños directos, indirectos, incidentales, especiales, consecuenciales o ejemplares, incluyendo pero no limitado a, daños por pérdida de beneficios, datos u otras pérdidas intangibles (incluso si NARANJA ERP AND ACADEMY S.A.S. ha sido advertido de la posibilidad de tales daños), resultantes del uso o la incapacidad de usar el servicio.'
+        ]
+    },
+    {
+        title: '8. Ley Aplicable',
+        subtext: [
+            'Estos Términos y Condiciones se regirán e interpretarán de acuerdo con las leyes de la República de Colombia. Cualquier disputa que surja en relación con estos términos será sometida a la jurisdicción exclusiva de los tribunales competentes en Colombia.'
+        ]
+    }
+]
+
+export default
+    {
+        TITLE,
+        TITLE_HOME,
+        CONTENT_HOME,
+        STRONG_TEXT_CONTENT_HOME,
+        TEXT_CONTENT_CARDS,
+        POLITICTS_TITLE,
+        PRIVACY_POLICY_CONTENT,
+        TERMS_AND_CONDITIONS_TITLE,
+        TERMS_AND_CONDITIONS_CONTENT
+    }
